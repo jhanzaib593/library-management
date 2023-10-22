2b7e795b82dc373dcd49c501fff7c4b82d063bc2
Jhanzaib <jhanzaib593@gmail.com>
2023-10-22 18:49:56 +0500

    first commit

---
+++ src/commpunter/Header/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import { Button } from 'antd';
+
+
+const header = () => {
+  return(
+    <>
+    <h1>jhanza</h1>
+    <Button type="">Button</Button>
+
+    </>
+  )
+};
+export default header;
