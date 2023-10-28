321fc797e1362a480cf5b3a3ba8a546de5c9eb44
Jhanzaib <jhanzaib593@gmail.com>
2023-10-28 15:14:12 +0500

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Header from "./commpunter/Header";
 import { ConfigProvider } from "antd";
+import Login from "./pages/login";
+import Signin from "./pages/signin";
 
 function App() {
   return (
-    <>
+    <div className="wappers">
       <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
         <Header />
+        <Signin />
+
       </ConfigProvider>
-    </>
+    </div>
   );
 }
 
--- src/commpunter/Header/index.js
+++ src/commpunter/Header/index.js
@@ -1,14 +1,20 @@
 import "./index.css";
-import { Button } from 'antd';
+import { Button, Col, Row } from "antd";
+// import Login from "../../pages/login";
 
-
-const header = () => {
-  return(
+const header = () => { 
+  return (
     <>
-    <h1>jhanza</h1>
-    <Button type="">Button</Button>
-
+      
+      <Row>
+        <Col span={12}></Col>
+        <Col span={12} justify="center" align="end">
+          <a href="../../pages/login/index.js"><Button>Login</Button></a>
+          <Button href="../../pages/signin/index.js">Sign In</Button>
+        </Col>
+      </Row>
+      
     </>
-  )
+  );
 };
 export default header;
